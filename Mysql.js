2d48d747ad594ac79f8cd7505eb40cf60f834094
Zainur <139621254+LawranceDevaz@users.noreply.github.com>
2026-04-25 10:19:09 +0700

    Update Mysql.js

--- Mysql.js
+++ Mysql.js
@@ -1,20 +1,17 @@
-const MySQL = require("mysql2"); // Pastikan sudah install mysql2
-const config = require("./config.json");
+const MySQL = require("mysql2");
+const config = require("./config.json")
 
-let MysqlMerpati = MySQL.createPool({
-    ...config.mysql,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
+let MysqlMerpati = MySQL.createPool(config.mysql)
 MysqlMerpati.getConnection((err, connect) => {
-    if (connect) {
-        console.log("\x1b[36m[MYSQL]: \x1b[0mDatabase MySql telah berhasil terhubung!");
-        connect.release();
+    if(err) {
+        console.log("\x1b[36m[MYSQL]: \x1b[0mDatabase MySql tidak dapat terhubung!")
+        console.log("\x1b[31m[ERROR]: \x1b[0m", err.message)
         return;
     }
-    console.log("\x1b[31m[MYSQL ERROR]: \x1b[0m" + (err ? err.message : "Tidak dapat terhubung!"));
-});
+    if(connect) {
+        console.log("\x1b[36m[MYSQL]: \x1b[0mDatabase MySql telah berhasil terhubung!");
+        connect.release();
+    }
+})
 
 module.exports = MysqlMerpati;
